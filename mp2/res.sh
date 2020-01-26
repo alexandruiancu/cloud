2cac0f6c81937ec9a6c299c3026ee91f3a075d12
Alexandru IANCU <alexandruiancu@users.noreply.github.com>
2020-01-26 14:27:42 +0200

    distributed hash table

---
+++ mp2/res.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#grep 'ANDU DEBUG @@'  ./dbg.log | grep initializing | grep "\<13\>"
+#grep 'ANDU DEBUG @@'  ./dbg.log | grep incrementing | grep "\<13\>"
+grep 'ANDU DEBUG @@ log ops' ./dbg.log
+ 
